feac4a3b954fd6dcdaa217fdf09606d190a3d212
vihangarasanjali <rvihanga0506@gmail.com>
2025-07-08 18:34:42 +0530

    updated order details

--- pages/orders/[orderid].tsx
+++ pages/orders/[orderid].tsx
@@ -1,17 +1,33 @@
 import SEO from '../../components/SEO';
 import OrderDetails from '../../components/OrderDetails';
-import { NextPage } from 'next';
+import { NextPage, GetServerSideProps } from 'next';
 import { homeConfig } from '../../utils';
 
-const OrderDetailsPage: NextPage = () => {
+interface Props {
+  orderId: string;
+}
+
+const OrderDetailsPage: NextPage<Props> = ({ orderId }) => {
   return (
     <>
-      <SEO {...{...homeConfig, title: 'Order Details | Your Store', description: 'View your order details and tracking information'}} />
+      <SEO {...{ ...homeConfig, title: 'Order Details | Your Store', description: 'View your order details and tracking information' }} />
       <main className="wrapper py-5">
-        <OrderDetails />
+        <OrderDetails orderId={orderId} />
       </main>
     </>
   );
 };
+
+// This runs on server and passes orderId as prop to the page
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { orderid } = context.params!; // note lowercase 'orderid'
+
+  return {
+    props: {
+      orderId: orderid as string, // pass it as 'orderId' prop to page
+    },
+  };
+};
+
 
 export default OrderDetailsPage;
