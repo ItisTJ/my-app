cb6fed6c38fcd56cdad7fb0eef38dfa4b3f89c33
vihangarasanjali <rvihanga0506@gmail.com>
2025-07-22 20:03:01 +0530

    Product card css

--- components/Products/Item/index.tsx
+++ components/Products/Item/index.tsx
@@ -16,7 +16,7 @@ const Item: React.FC<ProductInterface> = ({
     // Main container for the product card
     <div
     
-      className="my-3 p-3 rounded cursor-pointer bg-white shadow hover:shadow-md transition-shadow h-full flex flex-col sm:flex-row md:flex-col lg:flex-row xl:flex-col"
+      className="my-3 p-3 rounded cursor-pointer bg-purple-50 shadow hover:shadow-md transition-shadow h-full flex flex-col sm:flex-row md:flex-col lg:flex-row xl:flex-col"
       role="button" // Makes the card feel like a clickable item (accessibility)
     >
       {/* Product Image with link to product details page */}
